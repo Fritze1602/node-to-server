6167d3d95372b71fbf38ffa5797093678639469c
Friedrich Siever <friedrich.siever@gmail.com>
2019-03-01 15:18:19 +0100

    first deploy Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 
@@ -50,6 +52,6 @@ app.get('/bad', (req, res)=> {
 
 // Static Server
 
-app.listen(3000, () => {
-    console.log('Server is up on 3000');
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
 });
